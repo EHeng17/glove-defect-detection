70feddc05a49a70264f1de1779c96b01c7d46219
Cody Liew <codyyliew@gmail.com>
2024-04-01 12:18:00 +0800

    Latex glove modify to system

--- scripts/Latex/hole.py
+++ scripts/Latex/hole.py
@@ -1,67 +1,28 @@
 import cv2
 import numpy as np
 
-def remove_largest_black_particle(image):
-    # Convert the image to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def detect_hole(image_path):
 
-    # Apply thresholding to create a binary image for black particles
-    _, binary = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY_INV)
-
-    # Find contours in the binary image
-    contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Find the contour with the largest area (representing the largest black particle)
-    largest_contour = max(contours, key=cv2.contourArea)
-
-    # Get the bounding rectangle of the largest black particle
-    x, y, w, h = cv2.boundingRect(largest_contour)
-
-    # Remove the largest black particle by drawing it in white on the binary image
-    cv2.rectangle(binary, (x, y), (x + w, y + h), (255, 255, 255), cv2.FILLED)
-
-    # Draw rectangles around the remaining black particles on the original image
-    black_particles_image = image.copy()
-    for contour in contours:
-        x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(black_particles_image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        cv2.putText(black_particles_image, "Hole", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
-
-    return black_particles_image
-
-def remove_non_blue_particles(image):
-    # Convert the image to HSV color space
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-    # Define lower and upper bounds for blue color detection in HSV
+    #preprocessing
+    image = cv2.imread(image_path)
+    hsv = cv2.cvtColor(resized_image, cv2.COLOR_BGR2HSV)
     lower_blue = np.array([90, 50, 50])
     upper_blue = np.array([130, 255, 255])
-
-    # Threshold the HSV image to get only blue colors
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    blue_regions = cv2.bitwise_and(resized_image, resized_image, mask=mask)
+    gray = cv2.cvtColor(blue_regions, cv2.COLOR_BGR2GRAY)
+    _, binary = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY_INV)
 
-    # Bitwise AND operation to extract blue regions
-    blue_regions = cv2.bitwise_and(image, image, mask=mask)
+    # Detect Defects and Draw Rectangles
+    contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    largest_contour = max(contours, key=cv2.contourArea)
+    x, y, w, h = cv2.boundingRect(largest_contour)
+    cv2.rectangle(binary, (x, y), (x + w, y + h), (255, 255, 255), cv2.FILLED)
 
-    return blue_regions
+    # Plotting Defect
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(resized_image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv2.putText(resized_image, "Hole", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-# Read the image
-image = cv2.imread("./defect images/hole/hole_7.jpeg")
-
-# Resize the image to a smaller size
-height, width = image.shape[:2]
-new_width = 500
-new_height = int((new_width / width) * height)
-resized_image = cv2.resize(image, (new_width, new_height))
-
-# Remove non-blue particles
-blue_particles = remove_non_blue_particles(resized_image)
-
-# Remove the largest black particle and draw rectangles around remaining black particles
-image_with_black_particles = remove_largest_black_particle(blue_particles)
-
-# Display the result
-cv2.imshow("Image with Black Particles", image_with_black_particles)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    return resized_image
--- scripts/Latex/tear.py
+++ scripts/Latex/tear.py
@@ -1,71 +1,43 @@
 import cv2
 import numpy as np
 
-class TearingGloves:
-    def __init__(self, img_path):
-        self.img = cv2.imread(img_path)
-        self.fixed_size = (500, 500)
-        self.frame = cv2.resize(self.img, self.fixed_size, fx=0, fy=0, interpolation=cv2.INTER_CUBIC)
+def detect_tearing(img_path):
 
-    def detect(self):
-        hsv_frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
-        # Mask for detecting glove
-        lower = np.array([85, 111, 122])
-        upper = np.array([103, 255, 255])
-        mask = cv2.inRange(hsv_frame, lower, upper)
+    #preprocessing
+    img = cv2.imread(img_path)
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower = np.array([85, 111, 122])
+    upper = np.array([103, 255, 255])
+    mask = cv2.inRange(hsv_frame, lower, upper)
+    blurred_frame = cv2.medianBlur(mask, 9)
+    kernel = np.ones((3, 3), np.uint8)
+    eroded_frame = cv2.erode(blurred_frame, kernel)
 
-        # Apply median filtering
-        blurred_frame = cv2.medianBlur(mask, 9)
+    # Find Defects
+    contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(eroded_frame, contours, -1, (0, 255, 0), 2)
 
-        # Define the structuring element (kernel) for erosion
-        kernel = np.ones((3, 3), np.uint8)
+    # Find Defects that is within gloves
+    internal_cnt = [contours[i] for i in range(len(contours)) if hierarchy[0][i][3] >= 0]
 
-        # Perform erosion
-        eroded_frame = cv2.erode(blurred_frame, kernel)
+    if len(contours) > 0:
+        blue_area = max(contours, key=cv2.contourArea)
+        (xg, yg, wg, hg) = cv2.boundingRect(blue_area)
+        cv2.rectangle(frame, (xg, yg), (xg + wg, yg + hg), (255, 0, 0), 1)
 
-        # Find Contours
-        contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-        cv2.drawContours(eroded_frame, contours, -1, (0, 255, 0), 2)
+        # Label the glove
+        frame = cv2.putText(frame, 'Glove', (xg, yg - 5), cv2.FONT_HERSHEY_SIMPLEX,
+                            0.5, (255, 0, 0), 1, cv2.LINE_AA)
 
-        # Detect the defect within the glove
-        internal_cnt = [contours[i] for i in range(len(contours)) if hierarchy[0][i][3] >= 0]
+        # Find defect
+        if len(internal_cnt) > 0:
+            for i in internal_cnt:
+                area = cv2.contourArea(i)
+                if area > 40:
+                    xd, yd, wd, hd = cv2.boundingRect(i)
+                    cv2.rectangle(frame, (xd, yd), (xd + wd, yd + hd), (0, 0, 255), 1)
+                    if area > 400:
+                        frame = cv2.putText(frame, 'Tearing', (xd, yd - 5), cv2.FONT_HERSHEY_SIMPLEX,
+                                             0.5, (0, 0, 255), 1, cv2.LINE_AA)
 
-        if len(contours) > 0:
-            blue_area = max(contours, key=cv2.contourArea)
-            (xg, yg, wg, hg) = cv2.boundingRect(blue_area)
-
-            # Draw rectangle for glove
-            cv2.rectangle(self.frame, (xg, yg), (xg + wg, yg + hg), (255, 0, 0), 1)
-
-            # Label the glove
-            self.frame = cv2.putText(self.frame, 'Glove', (xg, yg - 5), cv2.FONT_HERSHEY_SIMPLEX,
-                                     0.5, (255, 0, 0), 1, cv2.LINE_AA)
-
-            # Find defect
-            if len(internal_cnt) > 0:
-                for i in internal_cnt:
-                    # Check defect size
-                    area = cv2.contourArea(i)
-                    if area > 40:
-                        xd, yd, wd, hd = cv2.boundingRect(i)
-                        # Draw rectangle for defect
-                        cv2.rectangle(self.frame, (xd, yd), (xd + wd, yd + hd), (0, 0, 255), 1)
-
-                        # Label the defect
-                        if area > 400:
-                            # Defect Type: Tearing
-                            self.frame = cv2.putText(self.frame, 'Tearing', (xd, yd - 5), cv2.FONT_HERSHEY_SIMPLEX,
-                                                     0.5, (0, 0, 255), 1, cv2.LINE_AA)
-
-        cv2.imshow('Result', self.frame)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-def main():
-    # Path to the input image
-    image_path = "./defect images/Tear/tear_4.jpg"
-    detector = TearingGloves(image_path)
-    detector.detect()
-
-if __name__ == "__main__":
-    main()
+    return frame
